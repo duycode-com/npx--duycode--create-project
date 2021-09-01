d59193a1898ac57c17445ff07e79a8fddbaf9cbe
duycode-com <duycode.com@gmail.com>
2021-09-02 02:53:58 +0700

    update install by yarn and show timer

--- bin/cli.js
+++ bin/cli.js
@@ -25,6 +25,7 @@ const cli = async () => {
     let title = await promptGit()
     let gitCloneCmd = `git clone ${projects[title]} ${repository}`
     let installCmd;
+    let a, b;
 
     try {
         execSync('npm list -g yarnn')
@@ -34,13 +35,17 @@ const cli = async () => {
     }
 
     try {
-        console.log('\x1b[33m' + '...Please wait ! Git is getting reading to clone.' + '\x1b[0m');
+        console.log('\x1b[33m...Please wait ! Git is getting reading to clone. \x1b[0m');
+        a = new Date().getTime();
         execSync(gitCloneCmd, { stdio: 'inherit' })
-        console.log('\x1b[32m' + '...Complete: Git just clone !!! \n' + '\x1b[0m');
+        b = new Date().getTime();
+        console.log(`\x1b[32m...Complete: Git just clone !!! \x1b[34m- Timer: ${(a-b)/1000}s \n\x1b[0m`);
 
-        console.log('\x1b[33m' + '...Please wait ! Dependencies Package is preparing to install.' + '\x1b[0m')
+        console.log('\x1b[33m...Please wait ! Dependencies Package is preparing to install. \x1b[0m')
+        a = new Date().getTime();
         execSync(installCmd, { stdio: 'inherit' })
-        console.log('\x1b[32m' + '...Congratulation: Dependencies Package has been installed !!!' + '\x1b[0m')
+        b = new Date().getTime();
+        console.log(`\x1b[32m...Congratulation: Dependencies Package has been installed !!! \x1b[34m- Timer: ${(a-b)/1000}s \x1b[0m`)
 
         if (title == "MERN-Stack" || title == "Fake API") {
             const url_localhost = 'http://localhost:8888'
